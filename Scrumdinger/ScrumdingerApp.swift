a6188223a5f629b24e4c2478a161224caebcc871
aangelozi <aangelozi@ciandt.com>
2022-07-05 17:06:31 -0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Alessandro Angelozi Ribeiro on 13/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
